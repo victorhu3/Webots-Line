4234f445ccf530faef83d193e5e93e14b6833fa5
Jeffrey Cheng <jeffreycheng1230@gmail.com>
2021-01-28 22:25:40 -0500

    power of 2

--- line_generator/LineGenerator.py
+++ line_generator/LineGenerator.py
@@ -4,26 +4,9 @@ numCol = int(input('Number of Col: '))
 worldName = input('Name of World (exclude .wbt): ')
 
 print('\nFor tile numbers: Tile 0 = top left corner, Tile 1 = 1 tile to the right, etc.')
-numCheckpoint = int(input('\nNumber of checkpoints: '))
 header = open('worldHeader.txt', 'r')
 file = open('../worlds/' + worldName + '.wbt', 'w')
 file.write(header.read())
-
-file.write('DEF Checkpoints Group {\n  children [\n')
-for x in range(numCheckpoint):
-    tile = int(input('Checkpoint ' + str(x + 1) + ' tile number: '))
-    direction = input('Checkpoint ' + str(x + 1) + ' direction: ')
-    if x == 0:
-        tileBetween = str(input('Input the number of tiles between start tile and checkpoint 1: '))
-    else:
-        tileBetween = str(input('Input the number of tiles between checkpoints ' + str(x) + ' and ' + str(x + 1) + ': '))
-
-    file.write('    Solid {\n')
-    file.write('      translation ' + str(int(tile % numCol) * 0.3 - 0.12) + ' 0 ' + str(int(tile / numCol) * 0.3 - 0.12) + '\n')
-    file.write('      children [\n        Shape {\n          appearance Appearance {\n            material Material {\n              diffuseColor 1 0.666667 0\n            }\n          }\n          geometry Cylinder {\n            height 0.02\n            radius 0.03\n          }\n        }\n      ]\n')
-    file.write('      name \"checkpoint' + str(x) + '\"\n')
-    file.write('      description \"' + direction.upper() + tileBetween + '\"\n    }\n')
-file.write('  ]\n}\n')
 
 print('\nOpen the Webots-Line\\tiles folder')
 print('Each image has a number for its name')
@@ -62,6 +45,34 @@ for x in range(numRow):
         file.write('            size ' + str(tileSize) + ' ' + str(tileSize) + '\n') #tile size
         file.write('          }\n        }\n      ]\n      name \"solid' + str(x * numCol + y) + '\"\n    }\n')
 file.write('  ]\n}')
+
+numCheckpoint = int(input('\n\nNumber of checkpoints: '))
+file.write('DEF Checkpoints Group {\n  children [\n')
+for x in range(numCheckpoint):
+    tile = int(input('Checkpoint ' + str(x + 1) + ' tile number: '))
+    direction = input('Checkpoint ' + str(x + 1) + ' direction: ')
+    if x == 0:
+        tileBetween = str(input('Input the number of tiles between start tile and checkpoint 1: '))
+    else:
+        tileBetween = str(input('Input the number of tiles between checkpoints ' + str(x) + ' and ' + str(x + 1) + ': '))
+
+    file.write('    Solid {\n')
+    file.write('      translation ' + str(int(tile % numCol) * 0.3 - 0.12) + ' 0 ' + str(int(tile / numCol) * 0.3 - 0.12) + '\n')
+    file.write('      children [\n        Shape {\n          appearance Appearance {\n            material Material {\n              diffuseColor 1 0.666667 0\n            }\n          }\n          geometry Cylinder {\n            height 0.02\n            radius 0.03\n          }\n        }\n      ]\n')
+    file.write('      name \"checkpoint' + str(x) + '\"\n')
+    file.write('      description \"' + direction.upper() + tileBetween + '\"\n    }\n')
+file.write('  ]\n}\n')
+
+numObs = int(input('\n\nNumber of obstacles: '))
+file.write('DEF Obstacles Group {\n  children [')
+for x in range(numObs):
+    tile = int(input('Obstacle ' + str(x + 1) + ' tile number: '))
+    file.write('    Solid {\n')
+    file.write('      translation ' + str(int(tile % numCol) * 0.3) + ' 0.06 ' + str(int(tile / numCol) * 0.3) + '\n')
+    file.write('      scale 1.3 1.3 1.3\n      children [\n        Shape {\n          appearance Appearance {\n            material Material {\n              diffuseColor 0.2 0.2 0.8\n            }\n          }\n          geometry Cylinder {\n            height 0.1\n            radius 0.05\n          }\n        }\n      ]\n')
+    file.write('      name \"obstacle' + str(x + 1) + '\"\n')
+    file.write('      boundingObject Shape {\n        appearance Appearance {\n          material Material {\n            diffuseColor 0.2 0.2 0.8\n          }\n        }\n        geometry Cylinder {\n          height 0.1\n          radius 0.05\n        }\n      }\n      physics Physics {\n        mass 7\n      }\n    }\n')
+file.write(' ]\n}\n')
 
 file.close()
 header.close()
